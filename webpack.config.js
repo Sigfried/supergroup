035fcbd50efa8ddd67d681aefb4d13911dbe3718
Sigfried Gold <sigfried@sigfried.org>
2016-01-22 08:28:21 -0500

    cleanup

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    entry: './supergroup.js',
+    output: {
+        path: './',
+        filename: 'bundle.js',
+    },
+    resolve: {
+        modulesDirectories: ['node_modules', 'bower_components'],
+    },
+    //watch: true,
+    colors: true,
+    progress: true,
+    cache: true,
+    debug: true
+};
